64b3ceff180d9ea9473eef1e83508893d2c4cf2c
sanchezeinyel12-svg <sanchezeinyel12@gmail.com>
2026-04-25 14:40:42 -0600

    Update script.js

--- script.js
+++ script.js
@@ -65,7 +65,7 @@
       <div class="sorp-hearts">❤️ ❤️ ❤️</div>
 
       <!-- 📸 CAMBIA "TU_RUTA_AQUI" POR LA RUTA DE TU FOTO -->
-      <img class="sorp-foto" src="son.jpeg" alt="Foto">
+      <img class="sorp-foto" src="rebe3.jpeg" alt="Foto">
 
       <div class="sorp-titulo">Para ti, mi psicóloga preciosa</div>
       <div class="sorp-sub">Algo que quiero decirte antes que nada amorcito</div>
